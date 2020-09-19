43413aad6ed59d6f889168ed1aa74aa3561e1f70
lakergirl702 <jesshoran87@gmail.com>
2020-09-19 11:28:31 -0700

    added all routes

--- Challenges/regftr_app_api/app/controllers/gifts_controller.rb
+++ Challenges/regftr_app_api/app/controllers/gifts_controller.rb
@@ -13,9 +13,36 @@ class GiftsController < ApplicationController
         puts params
         puts "====params===="
         #Find the specific gift we are looking for
-        render json: Gift.find(params[:id])
+        gift = Gift.find(params[:id])
+        render json: {
+            status: 200, 
+            gift: gift
+        }
+    end
+    #made request in postman and it works!
+    def create
+        gift = Gift.new(gift_params)
+        if gift.save
+            render json: { status:201, gift: gift}
+        else
+            render json: {status: 422, song:song}
+        end
     end
 
+    def update
+        gift = Gift.find(params[:id])
+        gift.update(gift_params)
+        render json: { status: 200, gift: gift }
+    end
+    def destroy
+        gift = Gift.destroy(params[:id])
+        render json: {status: 204}
+    end
 
+    private
+
+    def gift_params
+        params.required(:gift).permit(:description, :value, :event, :gift_giver)
+    end
 
 end
--- Challenges/regftr_app_api/app/models/gift.rb
+++ Challenges/regftr_app_api/app/models/gift.rb
@@ -1,2 +1,3 @@
 class Gift < ApplicationRecord
+    # validates :description, presence: true
 end
