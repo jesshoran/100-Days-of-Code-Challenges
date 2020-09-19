6cea8dd6b697eb75949856171480e21e85aebaeb
lakergirl702 <jesshoran87@gmail.com>
2020-09-19 10:32:42 -0700

    controller created, index and show working

--- Challenges/regftr_app_api/app/controllers/gifts_controller.rb
+++ Challenges/regftr_app_api/app/controllers/gifts_controller.rb
@@ -1,2 +1,21 @@
-class GiftsController < Application GiftsController
+class GiftsController < ApplicationController
+    def index
+        puts "====params===="
+        puts params
+        puts "====params===="
+        render json: {
+            status: 200, 
+            gifts: Gift.all
+        }
+    end
+    def show
+        puts "====params===="
+        puts params
+        puts "====params===="
+        #Find the specific gift we are looking for
+        render json: Gift.find(params[:id])
+    end
+
+
+
 end
