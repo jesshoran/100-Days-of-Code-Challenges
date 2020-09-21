4c5662944cac024a73b1433badfa1acf1bd7f564
lakergirl702 <jesshoran87@gmail.com>
2020-09-21 14:51:23 -0700

    react module and controller

--- Challenges/dating_app/dating_app_client/src/App.js
+++ Challenges/dating_app/dating_app_client/src/App.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 import Daters from './components/Hello.js'
 
 export default function App() {
--- Challenges/dating_app/dating_app_client/src/components/Hello.js
+++ Challenges/dating_app/dating_app_client/src/components/Hello.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
 export default function Daters (props) {
+
+    const getDaters = async () => {
+        try{
+        const response = fetch('localhost:3000/users')
+        const daters = await response.json();
+        console.log(daters);
+        }catch(error){
+         console.error(error);
+         }
+       }
+       
     return (
         <div className="App">
-              <h1>Hello Dating World</h1>      
+          <h1>Hello Dating World</h1>   
+             
         </div>
       );
   }
