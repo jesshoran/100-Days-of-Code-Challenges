01555b35ce90f5be1788625c8cfdfa23ed3bde96
lakergirl702 <jesshoran87@gmail.com>
2020-09-21 14:29:04 -0700

    made react app

---
+++ Challenges/dating_app/dating_app_client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Daters from './components/Hello.js'
+
+export default function App() {
+  return (
+      <div className="App">
+        <Daters />
+      </div>
+    );
+  }
+
---
+++ Challenges/dating_app/dating_app_client/src/components/Hello.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Daters (props) {
+    return (
+        <div className="App">
+              <h1>Hello Dating World</h1>      
+        </div>
+      );
+  }
