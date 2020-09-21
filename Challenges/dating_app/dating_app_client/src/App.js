515923e5762eeb9b3f9e56e0997bbc224159e14f
lakergirl702 <jesshoran87@gmail.com>
2020-09-21 16:55:48 -0700

    flexbox layout

--- Challenges/dating_app/dating_app_client/src/App.js
+++ Challenges/dating_app/dating_app_client/src/App.js
@@ -4,7 +4,14 @@ import Daters from './components/Hello.js'
 export default function App() {
   return (
       <div className="App">
-        <Daters />
+        <div className="container">
+          <nav><Daters /></nav>
+          <main>
+          <h1>Hello Dating World</h1> 
+        </main>
+        <aside></aside>
+        </div>
+        <footer/>
       </div>
     );
   }
--- Challenges/dating_app/dating_app_client/src/components/Hello.js
+++ Challenges/dating_app/dating_app_client/src/components/Hello.js
@@ -1,21 +1,50 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 export default function Daters (props) {
+    const [daters, setDaters] = useState([])
+
+
 
     const getDaters = async () => {
         try{
-        const response = fetch('localhost:3000/users')
+        const response = await fetch('http://localhost:3000/users')
         const daters = await response.json();
+        setDaters(daters)
         console.log(daters);
         }catch(error){
          console.error(error);
          }
        }
 
+       useEffect(
+        ()=>{
+          const callDaters = async ()=>{
+           await getDaters();	
+         }
+         callDaters();
+        }, 
+        []
+        )
+
     return (
         <div className="App">
-          <h1>Hello Dating World</h1>   
-             
+  
+          {
+        daters.map( dater =>{
+          return (
+            <div key={dater.id} className="dater">
+              <h3>Name: {dater.name}</h3>
+              <img src={dater.img} alt={dater.name}/>
+              <h4>Starsign: {dater.starsign} </h4>
+              <h4>Age: {dater.age} </h4>
+              {dater.ltl ?
+                <h5> Loves to laugh and have a good ol time </h5> :
+                <h5>Hates laughing, doesn't like fun</h5>
+              }
+            </div>
+          )
+        })
+      }
         </div>
       );
   }
