a56cc9d4e299d5188c15afcf38b1da3d90ca2a18
lakergirl702 <jesshoran87@gmail.com>
2020-09-22 16:47:48 -0700

    frontend done

--- Challenges/icescript_icecream/icescript_icecream_client/src/App.js
+++ Challenges/icescript_icecream/icescript_icecream_client/src/App.js
@@ -3,15 +3,30 @@ import LineChart from "./components/LineChart.js";
 import "./App.css";
 
 const App = () => {
-
-  const getData = () => {
-    fetch('/ice_creams')                                        
-      .then(response => response.json())                                            
-      .then(json => console.log(json))                                              
-      .catch(err => console.log(err))  
+  const prepareData = (data) => {
+    const chartData = {
+      labels: [],
+      datasets: [
+        {
+          label: 'Avg high temps',
+          data: []
+        }
+      ]
+    }
+  
+    data.temperatures.forEach(temperature => {
+      chartData.labels.push(temperature.month)
+      chartData.datasets[0].data.push(temperature.average_high_f)
+    })
+    return chartData
   }
   
-  getData();
+  const getData = () => {
+    fetch("/locations/1")
+      .then((response) => response.json())
+      .then((jData) => prepareData(jData))
+      .catch((err) => console.log(err));
+  };
 
   return (
     <div className="App">
