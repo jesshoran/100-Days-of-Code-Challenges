0f7c9e71ea1f0860813d27b5f55f58f78fc73537
lakergirl702 <jesshoran87@gmail.com>
2020-10-29 16:34:44 -0700

    refactored original problem

--- Challenges/udemy/udemy-fcounter.js
+++ Challenges/udemy/udemy-fcounter.js
@@ -26,3 +26,32 @@ let test1 = [1, 2, 3, 2]
 let test2 = [9, 1, 4, 4]
 
 console.log(same(test1, test2))
+
+
+//REFACTOR
+//use frequency counter to check each value 
+
+const sameSame = (arr1, arr2) => {
+    if (arr1.length !== arr2.length) {
+        return false;
+    } 
+    let frequencyCounter1 = {}
+    let frequencyCounter2 = {}
+    for (let value of arr1) {
+        frequencyCounter1[value] = frequencyCounter1[value] +1 || 1;
+    }
+    for (let value of arr2) {
+        frequencyCounter2[value] = frequencyCounter2[value] +1 || 1;
+    }
+    //check and see if the values in freq counter 2 are squared the first
+    for(let key in frequencyCounter1) {
+        if (!(key ** 2 in frequencyCounter2)) {
+            return false
+        } if (frequencyCounter2[key **2] !== frequencyCounter1[key]) {
+            return false
+        }                      
+    }
+    return true
+}
+
+console.log(sameSame([1,2,3,2,5], [9,1,4,4,11]))
