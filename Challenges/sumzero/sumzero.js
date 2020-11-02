3cde7283e6a711e3c69c0ff9faaeaa99b11501e9
lakergirl702 <jesshoran87@gmail.com>
2020-11-02 15:20:15 -0800

    count unique values completed

--- Challenges/sumzero/sumzero.js
+++ Challenges/sumzero/sumzero.js
@@ -34,3 +34,21 @@ function sumZeroBetter(arr) {
 }
 
 console.log(sumZeroBetter([-4,-3,-2,-1,0,1,2,5,10]))
+
+//Implement a function called countUniqueValues which accpets a sorted array and counts the unique values in the array
+//there can be negative numbers in the array but it will always be sorted.
+
+
+function countUniqueValues (arr) {
+ let i = 0;
+ for (let j = 1; j < arr.length; j++) {
+    if (arr[i] !== arr[j]) {
+        i++;
+        arr[i] = arr[j]
+    }
+    console.log(i, j)
+ }
+ return i + 1;
+}
+
+console.log(countUniqueValues([1, 1, 2, 3, 3, 4, 5, 6, 6, 7]))
