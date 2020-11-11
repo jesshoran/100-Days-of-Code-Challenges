80eacfcd6a98439e42aaacb6e3bf77068ef2775b
lakergirl702 <jesshoran87@gmail.com>
2020-11-11 11:18:35 -0800

    adding helper method

--- Challenges/udemy/udemy-recursion.js
+++ Challenges/udemy/udemy-recursion.js
@@ -22,3 +22,46 @@ function sumRange(num) {
 }
 
 console.log(sumRange(3))
+
+//not recursive
+function factorial(num) {
+    let total = 1;
+    for (let i = num; i > 1; i --) {
+        total *= i
+    }
+    return total
+}
+
+console.log(factorial(3))
+
+//recursive
+
+function factorial(num) {
+    if (num === 1) return 1;
+    return num + factorial(num - 1);
+}
+
+//HELPER
+
+function collectOddValues(arr){
+    
+    let result = [];
+
+    function helper(helperInput){
+        if(helperInput.length === 0) {
+            return;
+        }
+        
+        if(helperInput[0] % 2 !== 0){
+            result.push(helperInput[0])
+        }
+        
+        helper(helperInput.slice(1))
+    }
+    
+    helper(arr)
+
+    return result;
+}
+
+collectOddValues([1,2,3,4,5,6,7,8,9])
