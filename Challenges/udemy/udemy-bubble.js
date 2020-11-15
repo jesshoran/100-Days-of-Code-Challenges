e322ca92b64275f4f0165ec15e1143e4ce77dcb0
lakergirl702 <jesshoran87@gmail.com>
2020-11-15 11:56:01 -0800

    bubble sort completed, not optimized yet

---
+++ Challenges/udemy/udemy-bubble.js
@@ -0,0 +1,18 @@
+//not efficient version, O(N^2)
+
+function bubble(arr) {
+    for (let i = arr.length; i > 0; i--) {
+        for (let j = 0; j < i - 1; j++) {
+            console.log(arr, arr[j], arr[j+1])
+            if (arr[j] > arr[j + 1]) {
+            //SWAP
+            let temp = arr[j];
+            arr[j] = arr[j+1];
+            arr[j+1] = temp;
+            }
+        }
+    }
+    return arr
+}
+
+console.log(bubble([37, 45, 29, 8]))
