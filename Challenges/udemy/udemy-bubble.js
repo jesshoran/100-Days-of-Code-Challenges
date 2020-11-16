5c7cf3c7372bb8cdee760d152854a83de64ad5ca
lakergirl702 <jesshoran87@gmail.com>
2020-11-16 11:36:32 -0800

    bubble sort optimized

--- Challenges/udemy/udemy-bubble.js
+++ Challenges/udemy/udemy-bubble.js
@@ -1,16 +1,22 @@
 //not efficient version, O(N^2)
 
 function bubble(arr) {
+    let noSwaps;
     for (let i = arr.length; i > 0; i--) {
+        noSwaps = true;  //*optimized version*
+        //i accounts for how many left in the array to loop through
         for (let j = 0; j < i - 1; j++) {
-            console.log(arr, arr[j], arr[j+1])
+            //j is the position
+            // console.log(arr, arr[j], arr[j+1])
             if (arr[j] > arr[j + 1]) {
             //SWAP
             let temp = arr[j];
             arr[j] = arr[j+1];
             arr[j+1] = temp;
+            noSwaps = false;  //*optimized version*
             }
         }
+        if(noSwaps) break;  //*optimized version*
     }
     return arr
 }
