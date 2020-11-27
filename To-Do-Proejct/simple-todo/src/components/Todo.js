d012202ca05a69f50ed08eaec74eaee9b3a16c21
lakergirl702 <jesshoran87@gmail.com>
2020-11-26 16:07:40 -0800

    created db

---
+++ To-Do-Proejct/simple-todo/src/components/Todo.js
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from 'react';
+
+
+export default function Todo ( {todo, index, completeTodo, removeTodo, day} ) {
+
+
+    
+    return(
+      <div style={{textDecoration: todo.isCompleted ? 'line-through' : '' }}
+      className="to-do">
+        {todo.text}
+        <div>
+          <button onClick={() => completeTodo(index)}>Complete</button>
+          <button onClick={() => removeTodo(index)}>Delete</button>
+        </div>
+      </div>
+    )
+  }
+
+  
