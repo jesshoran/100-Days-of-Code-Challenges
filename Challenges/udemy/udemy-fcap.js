0c22fd579ee9e3815edbccc861380daa3e991bd6
lakergirl702 <jesshoran87@gmail.com>
2021-01-18 17:40:34 -0800

    frequency updated

--- Challenges/udemy/udemy-fcap.js
+++ Challenges/udemy/udemy-fcap.js
@@ -4,15 +4,20 @@
 
 function same(arr1, arr2) {
     //check to see if they're equal, if not we're done
-    if (arr1.length !== arr1.length) {
+    if (arr1.length !== arr2.length) {
         return false
     } 
     //see if the square of arr1[i] is in the second one, then splice it out and keep going through the loop
     //indexOf to see if it's in there (if it equals -1, it's not in there, so we know it's false)
     for (let i = 0; i < arr1.length; i++) {
        let correctIndex = arr2.indexOf(arr1[i] ** 2)
-       if (correctIndex = -1) {
+       if (correctIndex === -1) {
            return false
-       } arr2.splice(correctIndex, 1)
+       } arr2.splice(correctIndex,1)
     } return true
 }
+
+const test1 = [1, 2, 3, 4]
+const test2 = [16, 4, 9, 1]
+
+console.log(same(test1, test2))
